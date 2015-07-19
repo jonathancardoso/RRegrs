2839a61fbedf18d03c0875b3197076f3bee2f2f2
Egon Willighagen <egon.willighagen@gmail.com>
2015-07-20 00:15:28 +0200

    Added a first basic call (though it fails at this moment; addressing #10)

---
+++ RRegrs/tests/testthat/test-simpleCall.R
@@ -0,0 +1,19 @@
+library(RRegrs)
+context("simple")
+
+test_that("the chemical classics data can be loaded", {
+  chemClassicsData = system.file("extdata", "chemClassic.tsv", package = "RRegrs")
+  data = read.table(chemClassicsData, sep="\t", header=TRUE)
+  expect_equal(9, nrow(data))
+})
+
+test_that("the method doesn't fail for just MLR", {
+  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+  results = RRegrs(
+    DataFileName=dsData,
+    fLM="T",fGLM="F",fPLS="F",fLASSO="F",fSVRM="F",fNN="F",fRF="F",
+    fRFRFE="F",fSVMRFE="F",fENET="F",
+    trainFrac=0.75,iSplitTimes=3,noYrand=49,CVtypes="LOOCV"
+  )
+})
+
