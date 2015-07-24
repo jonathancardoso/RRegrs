54f3b9fae4a5b4a3733cc7642a6039de65fcc674
Egon Willighagen <egon.willighagen@gmail.com>
2015-07-24 10:28:10 +0200

    Added tests for most methods, GLM and SVMRFE are failing and commented out

--- RRegrs/tests/testthat/test-simpleCall.R
+++ RRegrs/tests/testthat/test-simpleCall.R
@@ -50,3 +50,93 @@ test_that("the method works for ds.gajewicz.csv", {
   )
 })
 
+test_that("the method works with both repeatedcv and LOOCV", {
+  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+  results = RRegrs(
+    DataFileName=dsData,
+    fLM="T",fGLM="F",fPLS="T",fLASSO="F",fSVRM="F",fNN="F",fRF="F",
+    fRFRFE="F",fSVMRFE="F",fENET="F",
+    trainFrac=0.75,iSplitTimes=3,noYrand=0,CVtypes="repeatedcv;LOOCV"
+  )
+})
+
+#test_that("the method works with GLM", {
+#  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+#  results = RRegrs(
+#    DataFileName=dsData,
+#    fLM="T",fGLM="T",fPLS="F",fLASSO="F",fSVRM="F",fNN="F",fRF="F",
+#    fRFRFE="F",fSVMRFE="F",fENET="F",
+#    trainFrac=0.75,iSplitTimes=3,noYrand=0,CVtypes="repeatedcv;LOOCV"
+#  )
+#})
+
+test_that("the method works with LASSO", {
+  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+  results = RRegrs(
+    DataFileName=dsData,
+    fLM="T",fGLM="F",fPLS="F",fLASSO="T",fSVRM="F",fNN="F",fRF="F",
+    fRFRFE="F",fSVMRFE="F",fENET="F",
+    trainFrac=0.75,iSplitTimes=3,noYrand=0,CVtypes="repeatedcv;LOOCV"
+  )
+})
+
+
+test_that("the method works with SVRM", {
+  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+  results = RRegrs(
+    DataFileName=dsData,
+    fLM="T",fGLM="F",fPLS="F",fLASSO="F",fSVRM="T",fNN="F",fRF="F",
+    fRFRFE="F",fSVMRFE="F",fENET="F",
+    trainFrac=0.75,iSplitTimes=3,noYrand=0,CVtypes="repeatedcv;LOOCV"
+  )
+})
+
+test_that("the method works with NN", {
+  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+  results = RRegrs(
+    DataFileName=dsData,
+    fLM="T",fGLM="F",fPLS="F",fLASSO="F",fSVRM="F",fNN="T",fRF="F",
+    fRFRFE="F",fSVMRFE="F",fENET="F",
+    trainFrac=0.75,iSplitTimes=3,noYrand=0,CVtypes="repeatedcv;LOOCV"
+  )
+})
+
+test_that("the method works with RF", {
+  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+  results = RRegrs(
+    DataFileName=dsData,
+    fLM="T",fGLM="F",fPLS="F",fLASSO="F",fSVRM="F",fNN="F",fRF="T",
+    fRFRFE="F",fSVMRFE="F",fENET="F",
+    trainFrac=0.75,iSplitTimes=3,noYrand=0,CVtypes="repeatedcv;LOOCV"
+  )
+})
+
+test_that("the method works with RFRFE", {
+  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+  results = RRegrs(
+    DataFileName=dsData,
+    fLM="T",fGLM="F",fPLS="F",fLASSO="F",fSVRM="F",fNN="F",fRF="F",
+    fRFRFE="T",fSVMRFE="F",fENET="F",
+    trainFrac=0.75,iSplitTimes=3,noYrand=0,CVtypes="repeatedcv;LOOCV"
+  )
+})
+
+#test_that("the method works with SVRMRFE", {
+#  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+#  results = RRegrs(
+#    DataFileName=dsData,
+#    fLM="T",fGLM="F",fPLS="F",fLASSO="F",fSVRM="F",fNN="F",fRF="F",
+#    fRFRFE="F",fSVMRFE="T",fENET="F",
+#    trainFrac=0.75,iSplitTimes=3,noYrand=0,CVtypes="repeatedcv;LOOCV"
+#  )
+#})
+
+test_that("the method works with ENET", {
+  dsData = system.file("extdata", "ds.gajewicz.csv", package = "RRegrs")
+  results = RRegrs(
+    DataFileName=dsData,
+    fLM="T",fGLM="F",fPLS="F",fLASSO="F",fSVRM="F",fNN="F",fRF="F",
+    fRFRFE="F",fSVMRFE="F",fENET="T",
+    trainFrac=0.75,iSplitTimes=3,noYrand=0,CVtypes="repeatedcv;LOOCV"
+  )
+})
