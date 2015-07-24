0283ac667d239d178176ed7ba7c41b3162995ed0
Egon Willighagen <egon.willighagen@gmail.com>
2015-07-24 08:57:32 +0200

    Make using the package the default, and added loading of caret in the non-library approach, addressing an error report by Cris

--- TEST/TestRRegrs.R
+++ TEST/TestRRegrs.R
@@ -3,12 +3,13 @@
 # -----------------------------------
 
 # Libraries and external custom functions
-# library(RRegrs)                 # load the RRegrs functions
+library(RRegrs)                 # load the RRegrs functions
 
-# using the version in the package:
-library(data.table)
-library(corrplot)
-source("../RRegrs/R/RRegrs_Functions.R")
+# if you downloaded a source distribution, you can also use the version in the package:
+#library(data.table)
+#library(caret)
+#library(corrplot)
+#source("../RRegrs/R/RRegrs_Functions.R")
 
 # ------------------------------
 # Default parameters of RRegrs
